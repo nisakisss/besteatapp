8b283f3ac9e6876d45f32bc735efd1501f0ff29c
Nazif Isaki <nazif.isaki@gmail.com>
2022-11-17 19:29:54 +0100

    add seed data, fast_jsonapu & serializers for review und restaurant

---
+++ app/serializers/restaurant_serializer.rb
@@ -0,0 +1,5 @@
+class RestaurantSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :image_url, :slug
+  has_many :reviews
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+restaurants = Restaurant.create([
+    {
+        name: "Hamburger House",
+        image_url: "https://via.placeholder.com/300"
+    },
+    {
+        name: "Pizza House",
+        image_url: "https://via.placeholder.com/300"
+    },
+    {
+        name: "Sushi House",
+        image_url: "https://via.placeholder.com/300"
+    }
+])
+
+reviews = Review.create([
+    {
+        title: "Great Restaurant",
+        description: "it tastes good",
+        score: 5,
+        restaurant: restaurants.first
+    },
+    {
+        title: "Baah",
+        description: "it doesn't taste good",
+        score: 1,
+        restaurant: restaurants.first
+    }
+])
